9f273f9c67a3443a4cb505c3f3e160689c388320
olavizz <149702276+olavizz@users.noreply.github.com>
2023-11-05 21:48:39 +0200

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,82 +1,56 @@
 import { useState } from 'react'
 
-const StatisticLine = (props) => {
-  return (
-      <tr>
-      <td>{props.text}</td>
-      <td>{props.value}</td>
-      </tr>
-  )
-}
 
-const Statistics = (props) => {
-  if (props.all !== 0) {
-  return (
-    <div>
-      <table>
-        <tbody>
-      <StatisticLine text="good" value={props.good}/>
-      <StatisticLine text="neutral" value={props.neutral} />
-      <StatisticLine text="bad" value={props.bad}/>
-      <StatisticLine text="all" value={props.all}/>
-      <StatisticLine text="average" value={(props.good - props.bad)/props.all}/>
-      <StatisticLine text="positive" value={props.good/props.all*100 + " %"}/>
-      </tbody>
-      </table>
-    </div>
-  )
-}
-return (
-  <div>
-    no feedback given
-  </div>
-)
-}
 
 const Button = (props) => {
     return (
       <button onClick={props.handleClick}>
-        {props.text}
+      {props.text}
       </button>
     )
 }
 
 const App = () => {
-  // tallenna napit omaan tilaansa
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const [all, setAll] = useState(0)
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+  const [votes, setVote] = useState([0, 0, 0, 0, 0, 0, 0, 0])
 
-  const goodfeedback = () => {
-    setGood(good + 1)
-    setAll(all + 1)
+  const number = () => {
+    let gg = Math.floor(Math.random()*8)
+    setSelected(gg)
   }
-  const neutralfeedback = () => {
-    setNeutral(neutral + 1)
-    setAll(all + 1)
+  const voting = () => {
+    const copy = [...votes]
+    copy[selected] += 1
+    setVote(copy)
   }
-  const badfeedback = () => {
-    setBad(bad + 1)
-    setAll(all + 1)
-  }
-
-
+  
+  
+  const mostVotes = Math.max(...votes)
+  const index = votes.indexOf(mostVotes)
 
   return (
     <div>
-      <div>
-        <h1> give feedback </h1>
-        <Button handleClick={goodfeedback} text='good'/>
-        <Button handleClick={neutralfeedback} text='neutral'/>
-        <Button handleClick={badfeedback} text='bad'/>
-      </div>
-      <h1>
-        statistics
-      </h1>
-      <Statistics good={good} neutral={neutral} bad={bad} all={all}/>
+    <div><h1>Anecdote of the day</h1></div>
+    <div>{anecdotes[selected]}</div>
+    <div>has {votes[selected]} votes</div>
+    <Button handleClick={voting} text='vote'/>
+    <Button handleClick={number} text='next anecdote'/>
+    <div><h1>Anecdote with most votes</h1></div>
+    <div>{anecdotes[index]}</div>
+    <div>has {votes[index]} votes</div>
+    
     </div>
   )
 }
-
 export default App
